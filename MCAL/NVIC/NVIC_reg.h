c5845c353dc051fa02a090324ec65cbdd56bcab0
Ahmed Othman <ahmed.othman.ghazzawi@gmail.com>
2023-04-29 16:30:27 +0300

    added comments

--- MCAL/NVIC/NVIC_reg.h
+++ MCAL/NVIC/NVIC_reg.h
@@ -9,31 +9,30 @@
 #define MCAL_NVIC_NVIC_REG_H_
 
 // Base Address 0xE000E100
-#define NVIC_BASE_ADDRESS		0xE000E100
+#define NVIC_BASE_ADDRESS 0xE000E100
 
 /* This is the enable of the NVIC */
 // NVIC Registers
-#define NVIC_ISER0      *((volatile u32*)(NVIC_BASE_ADDRESS + 0x000)) /* External Interrupts From 0 to 31 */
-#define NVIC_ISER1		*((volatile u32*)(NVIC_BASE_ADDRESS + 0x004)) /* External Interrupts From 32 to 63 */
-#define NVIC_ISER2		*((volatile u32*)(NVIC_BASE_ADDRESS + 0x008)) /* External Interrupts From 64 to 95 */
+#define NVIC_ISER0 *((volatile u32 *)(NVIC_BASE_ADDRESS + 0x000)) /* External Interrupts From 0 to 31 */
+#define NVIC_ISER1 *((volatile u32 *)(NVIC_BASE_ADDRESS + 0x004)) /* External Interrupts From 32 to 63 */
+#define NVIC_ISER2 *((volatile u32 *)(NVIC_BASE_ADDRESS + 0x008)) /* External Interrupts From 64 to 95 */
 
-#define NVIC_ICER0		*((volatile u32*)(NVIC_BASE_ADDRESS + 0x080)) /* External Interrupts From 0 to 31 */
-#define NVIC_ICER1		*((volatile u32*)(NVIC_BASE_ADDRESS + 0x084)) /* External Interrupts From 32 to 63 */
-#define NVIC_ICER2		*((volatile u32*)(NVIC_BASE_ADDRESS + 0x088)) /* External Interrupts From 64 to 95 */
+#define NVIC_ICER0 *((volatile u32 *)(NVIC_BASE_ADDRESS + 0x080)) /* External Interrupts From 0 to 31 */
+#define NVIC_ICER1 *((volatile u32 *)(NVIC_BASE_ADDRESS + 0x084)) /* External Interrupts From 32 to 63 */
+#define NVIC_ICER2 *((volatile u32 *)(NVIC_BASE_ADDRESS + 0x088)) /* External Interrupts From 64 to 95 */
 
-#define NVIC_ISPR0		*((volatile u32*)(NVIC_BASE_ADDRESS + 0x100))
-#define NVIC_ISPR1		*((volatile u32*)(NVIC_BASE_ADDRESS + 0x104))
-#define NVIC_ISPR2		*((volatile u32*)(NVIC_BASE_ADDRESS + 0x108))
+#define NVIC_ISPR0 *((volatile u32 *)(NVIC_BASE_ADDRESS + 0x100)) /* External Interrupts From 0 to 31 */
+#define NVIC_ISPR1 *((volatile u32 *)(NVIC_BASE_ADDRESS + 0x104)) /* External Interrupts From 32 to 63 */
+#define NVIC_ISPR2 *((volatile u32 *)(NVIC_BASE_ADDRESS + 0x108)) /* External Interrupts From 64 to 95 */
 
-#define NVIC_ICPR0		*((volatile u32*)(NVIC_BASE_ADDRESS + 0x180))
-#define NVIC_ICPR1		*((volatile u32*)(NVIC_BASE_ADDRESS + 0x184))
-#define NVIC_ICPR2		*((volatile u32*)(NVIC_BASE_ADDRESS + 0x188))
+#define NVIC_ICPR0 *((volatile u32 *)(NVIC_BASE_ADDRESS + 0x180)) /* External Interrupts From 0 to 31 */
+#define NVIC_ICPR1 *((volatile u32 *)(NVIC_BASE_ADDRESS + 0x184)) /* External Interrupts From 32 to 63 */
+#define NVIC_ICPR2 *((volatile u32 *)(NVIC_BASE_ADDRESS + 0x188)) /* External Interrupts From 64 to 95 */
 
-#define NVIC_IABR0		*((volatile u32*)(NVIC_BASE_ADDRESS + 0x200))
-#define NVIC_IABR1		*((volatile u32*)(NVIC_BASE_ADDRESS + 0x204))
-#define NVIC_IABR2		*((volatile u32*)(NVIC_BASE_ADDRESS + 0x208))
+#define NVIC_IABR0 *((volatile u32 *)(NVIC_BASE_ADDRESS + 0x200)) /* External Interrupts From 0 to 31 */
+#define NVIC_IABR1 *((volatile u32 *)(NVIC_BASE_ADDRESS + 0x204)) /* External Interrupts From 32 to 63 */
+#define NVIC_IABR2 *((volatile u32 *)(NVIC_BASE_ADDRESS + 0x208)) /* External Interrupts From 64 to 95 */
 
-
-#define NVIC_IPR0		*((volatile u8*)(NVIC_BASE_ADDRESS + 0x300))
+#define NVIC_IPR0 *((volatile u8 *)(NVIC_BASE_ADDRESS + 0x300))
 
 #endif /* MCAL_NVIC_NVIC_REG_H_ */
